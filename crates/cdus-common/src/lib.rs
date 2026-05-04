a83f8eacd84783b43a7e65c6a75a708abd1d4e1c
rohanakode490 <rohanakode52@gmail.com>
2026-05-04 23:18:01 +0530

    feat: add rust workspace crates

---
+++ crates/cdus-common/src/lib.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum IpcMessage {
+    Ping,
+    Pong,
+    Log(String),
+    ClipboardChanged(String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rmp_serde;
+
+    #[test]
+    fn test_ipc_message_roundtrip_json() {
+        let msg = IpcMessage::Log("test message".to_string());
+        let serialized = serde_json::to_string(&msg).unwrap();
+        let deserialized: IpcMessage = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(msg, deserialized);
+    }
+
+    #[test]
+    fn test_ipc_message_roundtrip_msgpack() {
+        let msg = IpcMessage::Ping;
+        let mut buf = Vec::new();
+        msg.serialize(&mut rmp_serde::Serializer::new(&mut buf)).unwrap();
+        
+        let mut de = rmp_serde::Deserializer::new(&buf[..]);
+        let deserialized: IpcMessage = Deserialize::deserialize(&mut de).unwrap();
+        assert_eq!(msg, deserialized);
+    }
+}
